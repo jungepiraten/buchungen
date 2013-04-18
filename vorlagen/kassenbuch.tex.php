106519d685e1ca1ffc85a3ace01cd02bb6a9ed15
root <root@verwaltung.aspiration.junge-piraten.de>
2013-04-18 08:48:46 +0200

    corrected saldovalues

--- vorlagen/kassenbuch.tex.php
+++ vorlagen/kassenbuch.tex.php
@@ -100,7 +100,7 @@
  \endfoot
 <?php if (isset($account["transactions"])) { $saldo = 0; foreach ($account["transactions"] as $buchung) { ?>
  \hline \textbf{<?php print($buchung["id"]) ?>} & <?php print($buchung["num"]) ?> & \multicolumn{4}{p{11cm}}{<?php print(latexSpecialChars($buchung["description"])) ?>} \\
-<?php foreach ($buchung["splits"] as $split) { if ($split["account_guid"] == $account["guid"]) { $saldo += $split["value"]; ?>
+<?php foreach ($buchung["splits"] as $split) { if ($split["account_guid"] == $account["guid"]) { $saldo -= $split["value"]; ?>
  \nopagebreak
  \multicolumn{2}{l}{\hspace{1cm}<?php print(date("d.m.Y", $buchung["date"])) ?>} & <?php print(latexSpecialChars($split["memo"])) ?> & <?php if ($split["value"] > 0) printf("%.2f EUR",$split["value"]) ?> & <?php if ($split["value"] < 0) printf("%.2f EUR",(-1)*$split["value"]) ?> & <?php printf("%.2f EUR",$saldo) ?> \\
 <?php } ?>
